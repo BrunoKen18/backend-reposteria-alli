5b2d5f158b0b880be43c03628bce4f8469e43b51
BrunoKen18 <157932899+BrunoKen18@users.noreply.github.com>
2024-01-28 12:16:24 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,8 @@
 import admin from 'firebase-admin';
+console.log('FIREBASE ENV:', process.env.FIREBASE);
+
 const serviceAccount = JSON.parse(process.env.FIREBASE as string);
-console.log("serviceAccount ",serviceAccount);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
