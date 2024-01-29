9c52cec206fc57f6c6ec7c99e5a8b71e62100318
BrunoKen18 <157932899+BrunoKen18@users.noreply.github.com>
2024-01-29 12:27:57 -0300

    Update airtable.ts

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -1,22 +1,8 @@
 import Airtable from 'airtable';
-import {tiendaIndex} from './algolia';
 var base = new Airtable({
   apiKey: process.env.AIRTABLE_KEY,
 }).base('appEt0LIcwvJBjgvB');
-const tiendaBase = new Airtable({apiKey: process.env.AIRTABLE}).base(
-  'appXu0aYFo1OsZRi0'
-);
-async function getDataAirtable() {
-  const newBase = tiendaBase('Furniture').select({
-    pageSize: 100,
-  });
-  const response = await newBase.all();
-  const object = response.map((r) => ({
-    objectID: r.id,
-    ...r.fields,
-  }));
-  const res = await tiendaIndex.replaceAllObjects(object);
-  return res.objectIDs;
-}
 
-export {base, getDataAirtable};
+
+
+export {base};
